2a50a5c5745d738795115eb82845b8ca4de7e398
Douglas Monteles <silvadouglas328@gmail.com>
2022-05-20 00:37:45 -0300

    CRUD of City

--- src/main/java/com/doug/agenda/controllers/FormCityController.java
+++ src/main/java/com/doug/agenda/controllers/FormCityController.java
@@ -1,95 +1,188 @@
 package com.doug.agenda.controllers;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import com.doug.agenda.controllers.contracts.IRegister;
+import com.doug.agenda.dao.GenericCrudDao;
+import com.doug.agenda.model.City;
+import com.doug.agenda.utils.Alert;
 import com.doug.agenda.utils.UF;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class FormCityController implements Initializable, IRegister {
 
-    @FXML
-    private Button btnDelete;
+	@FXML
+	private Label lbTitle;
 
-    @FXML
-    private Button btnNew;
+	@FXML
+	private Button btnDelete;
 
-    @FXML
-    private Button btnSave;
+	@FXML
+	private Button btnNew;
 
-    @FXML
-    private TableView<?> tableView;
+	@FXML
+	private Button btnSave;
 
-    @FXML
-    private JFXTextField tfDescription;
+	@FXML
+	private TableView<City> tableView;
 
-    @FXML
-    private JFXTextField tfId;
+	@FXML
+	private JFXTextField tfDescription;
 
-    @FXML
-    private TextField tfSearch;
+	@FXML
+	private JFXTextField tfId;
 
-    @FXML
-    private JFXTextField tfcep;
+	@FXML
+	private TextField tfSearch;
 
-    @FXML
-    private JFXComboBox<String> tfuf;
-    
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-    	tfuf.setItems(UF.ufGenerator());
-    }
+	@FXML
+	private JFXTextField tfcep;
 
-    @FXML
-    void deleteRegister(ActionEvent event) {
+	@FXML
+	private JFXComboBox<String> tfuf;
+	
+	private GenericCrudDao<City> cDao = new GenericCrudDao<>(City.class);
+	
+	private ObservableList<City> obsList = FXCollections.observableArrayList();
+	
+	private List<City> cities;
+	
+	private City selectedCity = new City();
 
-    }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		lbTitle.setText("Cadastro de cidade");
+		
+		createTableColumns();
+		updateTable();
+		setFormFields();
+		
+		tfuf.setItems(UF.ufGenerator());
+	}
 
-    @FXML
-    void includeRegister(ActionEvent event) {
+	@FXML
+	void deleteRegister(ActionEvent event) {
+		if (Alert.confirmAlert("Deseja realmente excluir a cidade: " + selectedCity.getDescription() + "?")) {
+			cDao.delete(selectedCity, selectedCity.getId());
+			
+			clearFormFields();
+			updateTable();
+			
+			Alert.informationAlert("Cidade excluída com sucesso!");
+		}
+	}
 
-    }
+	@FXML
+	void includeRegister(ActionEvent event) {
+		clearFormFields();
+	}
 
-    @FXML
-    void saveRegister(ActionEvent event) {
+	@FXML
+	void saveRegister(ActionEvent event) {
+		City city = new City();
+		
+		if (selectedCity != null) {
+			city.setId(selectedCity.getId());
+		}
+		
+		city.setCep(Long.valueOf(tfcep.getText()));
+		city.setDescription(tfDescription.getText());
+		city.setUf(tfuf.getValue());
+		
+		if (cDao.save(city)) {
+			Alert.informationAlert("Cidade cadastrada com sucesso!");
+		} else {
+			Alert.informationAlert("Erro ao cadastrar cidade");
+		}
+		
+		updateTable();
+		clearFormFields();
+	}
 
-    }
+	@FXML
+	void clickTable(MouseEvent event) {
+		setFormFields();
+	}
 
-    @FXML
-    void search(ActionEvent event) {
+	@FXML
+	void moveTable(KeyEvent event) {
+		setFormFields();
+	}
 
-    }
+	@FXML
+	void filterRegisters(KeyEvent event) {
+		updateTable();
+	}
 
 	@Override
 	public void createTableColumns() {
-		// TODO Auto-generated method stub
+		TableColumn<City, Long> idColumn = new TableColumn<>("ID");
 		
+		TableColumn<City, String> descriptionColumn = new TableColumn<>("DESCRIÇÃO");
+		
+		TableColumn<City, Long> cepColumn = new TableColumn<>("CEP");
+		
+		TableColumn<City, String> ufColumn = new TableColumn<>("UF");
+		
+		tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+		tableView.getColumns().addAll(idColumn, descriptionColumn, cepColumn, ufColumn);
+		
+		idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+		descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+		cepColumn.setCellValueFactory(new PropertyValueFactory<>("cep"));
+		ufColumn.setCellValueFactory(new PropertyValueFactory<>("uf"));
 	}
 
 	@Override
 	public void updateTable() {
-		// TODO Auto-generated method stub
+		obsList.clear();
 		
+		cities = cDao.findAll(tfSearch.getText());
+		cities.forEach(obsList::add);
+		
+		tableView.getItems().setAll(obsList);
+		tableView.getSelectionModel().selectFirst();
 	}
 
 	@Override
 	public void setFormFields() {
-		// TODO Auto-generated method stub
-		
+		if (!tableView.getItems().isEmpty()) {
+			int index = tableView.getSelectionModel().getSelectedIndex();
+			
+			selectedCity = tableView.getItems().get(index);
+			
+			tfId.setText(String.valueOf(selectedCity.getId()));
+			tfDescription.setText(selectedCity.getDescription());
+			tfcep.setText(String.valueOf(selectedCity.getCep()));
+			tfuf.setValue(selectedCity.getUf());
+		}
 	}
 
 	@Override
 	public void clearFormFields() {
-		// TODO Auto-generated method stub
+		selectedCity = null;
 		
+		tfId.clear();
+		tfDescription.clear();
+		tfcep.clear();
+		tfuf.getSelectionModel().select(-1);
 	}
 
 }
